19d4f87f46c40bfbb88608a6407aa913dc6f5e32
vucoffee2310 <vucoffee2310@gmail.com>
2025-08-24 23:41:42 +0700

    Update index.py

--- api/api/index.py
+++ api/api/index.py
@@ -64,7 +64,7 @@ def upload_youtube_audio():
             '-f', 'ba',
             '-S', '+abr,+tbr,+size',
             '--http-chunk-size', '9M',
-            '--limit-rate', '35M',
+            '--limit-rate', '20M',
             '--cookies', temp_cookie_file.name,
             '--extractor-args', extractor_args,
             '-o', '-',
